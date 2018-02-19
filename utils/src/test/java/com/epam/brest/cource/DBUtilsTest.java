a5f42295eefdb621be4c4831b6a930bf93e38333
xalf <Aliaksandr_Fedasiuk@epam.com>
2018-02-19 20:43:23 +0300

    Maven plugins

--- utils/src/test/java/com/epam/brest/cource/DBUtilsTest.java
+++ utils/src/test/java/com/epam/brest/cource/DBUtilsTest.java
@@ -1,12 +1,13 @@
 package com.epam.brest.cource;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import java.sql.SQLException;
 
 public class DBUtilsTest {
 
-    @org.junit.Test
+    @Test
     public void getConnection() throws SQLException, ClassNotFoundException {
         DBUtils dbUtils = new DBUtils();
         Assert.assertNotNull(dbUtils.getConnection());
