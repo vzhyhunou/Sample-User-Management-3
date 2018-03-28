db140812d07760c749eefc20ce6b42774fbaab3f
xalf <Aliaksandr_Fedasiuk@epam.com>
2018-03-28 19:00:50 +0300

    initial angular sample application

---
+++ orgchartmgr/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { DepartmentsComponent } from './components/departments/departments.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentsComponent,
+    EmployeesComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
